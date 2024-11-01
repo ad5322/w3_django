d19f070e189dca310bdac2af767fd236c010f294
ad5322 <arundeepak966@gmail.com>
2024-11-01 12:54:56 +0530

    static files

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
     mymembers = Member.objects.all().values()
@@ -24,12 +25,12 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
-    mymembers = Member.objects.all().values()
+    mydata = Member.objects.all().order_by('-firstname','id').values()
     template = loader.get_template('template.html')
     context = {
-        'fruits':['apple','mango','orange'],
-        'firstname':'Linus',
-        'mymembers':mymembers,
-        'greeting':1,
+        
+        'mymembers':mydata,
+        
     }
+    
     return HttpResponse(template.render(context,request))
